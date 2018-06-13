877b79a3b5b7967404f423e4afc862d01e5cc37f
Kevin Wittek <kevin.wittek@styracosoft.de>
2018-06-13 13:24:41 +0200

    Fix typo in tests

--- src/test/groovy/AcceptanceTest.groovy
+++ src/test/groovy/AcceptanceTest.groovy
@@ -39,7 +39,7 @@ class AcceptanceTest extends GebSpec {
         browser.go ("http://vote")
         sleep(1000)
 
-        then: "Both results show 100%"
+        then: "Both results show text%"
         $("button", 0).text() == "GROOVY"
         $("button", 1).text() == "KOTLIN"
     }
